ac8213ee6d47ba8d59fd27f661070d8c9774d83e
goropikari <goropikari56@gmail.com>
2021-03-28 23:18:29 +0900

    fix download script

--- script/setup.sh
+++ script/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl -fLo ps.zip https://www.morikita.co.jp/books/download/2357
-unzip ps.zip
-rm -f ps.zip
+curl -fLo irpb-files.zip https://www.morikita.co.jp/books/download/2357
+unzip irpb-files.zip
+rm -f irpb-files.zip
 
 curl -fLo jawiki.doc2vec.dmpv300d.tar.bz2 https://www.dropbox.com/s/njez3f1pjv9i9xj/jawiki.doc2vec.dmpv300d.tar.bz2?dl=0
+tar xf jawiki.doc2vec.dmpv300d.tar.bz2
+mv jawiki* irpb-files/data/
